85641c9263c2e63cbf91dd03383e263b5659ed41
Tim Fontes <italo5123@hotmail.com>
2022-11-02 13:15:15 -0300

    chore: :sparkles: runned fist rust code

---
+++ notes/1-rust-version.md
@@ -0,0 +1,5 @@
+* get rust version:
+    `rustc --version`
+* rustup is like nvm, it already comes out of the box with the rust lang toolkit
+
+
---
+++ src/rust/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut my_hello = "potato";
+    println!("{}", my_hello);
+    my_hello = "carrot";
+    println!("{}", my_hello);
+}
